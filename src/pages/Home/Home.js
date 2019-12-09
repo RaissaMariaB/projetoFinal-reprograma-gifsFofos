21d312a697f864894106a52bf272efc8384f902f
RaissaMariaB <raissamaria.sb@gmail.com>
2019-12-09 17:06:54 -0300

    adiciona alguns componentes e boostrap para nav

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navegation from '../../components/Nav/Navegation'
+
+class Home extends React.Component{
+    constructor(props){
+        super()
+    }
+render()
+
+
+
+
+
+}
+
+
+
+export default Home
