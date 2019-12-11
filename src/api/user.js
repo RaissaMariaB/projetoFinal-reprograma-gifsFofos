346dc8d69717efcb81dce662972c154a79ccb54c
RaissaMariaB <raissamaria.sb@gmail.com>
2019-12-11 18:28:39 -0300

    Adiciona axios a pasta api

--- src/api/user.js
+++ src/api/user.js
@@ -3,14 +3,14 @@ import config from './config'
 function getGif(nameGif){
     const url = `/search?api_key=dc6zaTOxFjmzC&q=${nameGif}&limit=1`
     return config.get(url)
-    console.log(url);
+   
     
 }
 
 
 
 
-export default getGif
+export { getGif}
 
 
 // http://api.giphy.com/v1/gifs/search?api_key=${minhaChave}&q=${oQueQueroBuscar}&limit=3`
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,79 +14,108 @@ import { getGif } from '../../api/user'
 import './style.css'
 
 
-class Home extends React.Component{
-    constructor(props){
+class Home extends React.Component {
+    constructor(props) {
         super(props)
         this.state = {
             value: '',
+            
+
         }
     }
 
     inputValue = e => {
         this.setState({
-          value: e.target.value
+            value: e.target.value
+
         })
-        console.log(this.state.value);        
-      }
+        console.log(this.state.value);
+    }
 
-
-    render(){
-        return(
+    searchGif   = () => {
+        if (this.state.value !== "") {
+          getGif(this.state.value)
+            .then(response => {
+                console.log(response);
+                
+              this.setState({
+                user: response.data
+              });
+              this.props.history.push({
+                pathname: "/gifs",
+                state: {
+                  user: this.state.user
+                }
+              });
+            })
+            .catch(error => {
+              this.props.history.push({
+                pathname: "/results",
+                state: {
+                  error: "User not found :( "
+                  //   error: error.response.data.message
+                }
+              });
+            });
+        }
+      };  
+    render() {
+                    return(
             <Fragment>
                 <Nav></Nav>
-                <Header/>
-                <section className= 'container__sobre'>
+                <Header />
+                <section className='container__sobre'>
                     <h1 className='sobre-h1'><img alt='Título da seção Sobre' className='title-sobre' src={titleSobre}></img></h1>
                     <hr />
-                    
-                    <div className= 'container__cards'>
-                    <Sobre
-                    imgSrc={Card1}
-                    children='as vezes só precisamos de um gif de gatinho para melhorarmos nosso dia.'
-                    altImg='foto de bonqeuinho fictício fofo segurando um coração'
-                    />
-                    <Sobre
-                     imgSrc={Card2}
-                     children='Pensando nisso, esse é um local mágico, onde as suas bads não tem vez, e apenas a fofurice é bem vinda!'
-                     altImg='foto de uma menina em desenho fazendo um coração com as mãos'
-                    />
-                    <Sobre
-                     className='ajuste-envelope'
-                     imgSrc={Card3}
-                     children='como no fim do dia tudo se resume a quem você quer alegrar com esse gif, te damos essa opção.'
-                     altImg='foto de um envelope com um coração cima'
-                     />
-                     </div>
-                </section>  
+
+                    <div className='container__cards'>
+                        <Sobre
+                            imgSrc={Card1}
+                            children='as vezes só precisamos de um gif de gatinho para melhorarmos nosso dia.'
+                            altImg='foto de bonqeuinho fictício fofo segurando um coração'
+                        />
+                        <Sobre
+                            imgSrc={Card2}
+                            children='Pensando nisso, esse é um local mágico, onde as suas bads não tem vez, e apenas a fofurice é bem vinda!'
+                            altImg='foto de uma menina em desenho fazendo um coração com as mãos'
+                        />
+                        <Sobre
+                            className='ajuste-envelope'
+                            imgSrc={Card3}
+                            children='como no fim do dia tudo se resume a quem você quer alegrar com esse gif, te damos essa opção.'
+                            altImg='foto de um envelope com um coração cima'
+                        />
+                    </div>
+                </section>
                 <section className='container__pesquise'>
                     <h1 className='title-search'>
                         Dá uma pesquisadinha
                     </h1>
-                    <hr/>  
+                    <hr />
                     <div className='container__input'>
-                    <p className='text-random2'>
-                    digite aqui um tema para gif 
+                        <p className='text-random2'>
+                            digite aqui um tema para gif
                     </p>
-                    <Search
-                    typing= {this.inputValue}
-                    />
-                    </div>                  
+                        <Search
+                            typing={this.inputValue}
+                        />
+                    </div>
                     <div className='container__buttom'>
                         <p className='text-random'>
-                            está na dúvida do que pesquisar? <br/> clica aqui que ajudamos
+                            está na dúvida do que pesquisar? <br /> clica aqui que ajudamos
                         </p>
-                        <Btn btnStyle='random-azul'>    
-                        random cuteness
-                       </Btn>                   
+                        <Btn btnStyle='random-azul' btnClick={this.searchGif}>
+                            random cuteness
+                       </Btn>
 
                     </div>
                 </section>       
 
-            </Fragment>
+            </Fragment >
         )
+        }
     }
-}
 
 
 
-export default Home
+    export default Home
